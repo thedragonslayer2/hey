cb3a8896edd168e44c95480f151c760c750e89c1
thedragonslayer2 <84556593+thedragonslayer2@users.noreply.github.com>
2021-08-17 22:22:07 -0700

    Add files via upload

---
+++ Misc./Time.lua
@@ -0,0 +1,22 @@
+local shit = { "Off", "Day", "Night" }
+game:GetService"RunService".RenderStepped:Connect(function()
+	if game:GetService"Lighting" and game:GetService"Lighting".ClockTime then
+		if shit.Night == true then game:GetService"Lighting".ClockTime = 3
+		elseif shit.Day == true then game:GetService"Lighting".ClockTime = 14 end
+	end
+end)
+
+function shit:Change(value)
+	if value == "Off" then
+		shit.Night = false
+		shit.Day = false
+	elseif value == "Day" then
+		shit.Night = false
+		shit.Day = true
+	elseif value == "Night" then
+		shit.Night = true
+		shit.Day = false
+	end
+end
+
+return shit
