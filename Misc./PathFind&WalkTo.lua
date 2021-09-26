2539576a62dea92178ac11ecb1bd8a4fb94d37fd
thedragonslayer2 <84556593+thedragonslayer2@users.noreply.github.com>
2021-09-26 16:15:01 +0800

    Update PathFind&WalkTo.lua

--- Misc./PathFind&WalkTo.lua
+++ Misc./PathFind&WalkTo.lua
@@ -1,42 +1,43 @@
 local shit = {}
 local PathfindingService = game:GetService"PathfindingService"
 local Players = game:GetService"Players"
-local path = PathfindingService:CreatePath()
 local player = Players.LocalPlayer
 local character = player.Character
 local humanoid = character:WaitForChild"Humanoid"
-local TEST_DESTINATION = Vector3.new(100, 0, 100)
 local waypoints
 local nextWaypointIndex
 local reachedConnection
 local blockedConnection
 
 function shit:followPath(destination)
-	local success, errorMessage = pcall(function()
-		path:ComputeAsync(character.PrimaryPart.Position, destination)
-	end)
-	if success and path.Status == Enum.PathStatus.Success then
-		waypoints = path:GetWaypoints()
-		blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
-			if blockedWaypointIndex >= nextWaypointIndex then
-				blockedConnection:Disconnect()
-				shit:followPath(destination)
-			end
+	pcall(function()
+		local path = PathfindingService:CreatePath()
+		local success, errorMessage = pcall(function()
+			path:ComputeAsync(character.PrimaryPart.Position, destination)
 		end)
-		if not reachedConnection then
-			reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
-				if reached and nextWaypointIndex < #waypoints then
-					nextWaypointIndex = nextWaypointIndex + 1
-					humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-				else
-					reachedConnection:Disconnect()
+		if success and path.Status == Enum.PathStatus.Success then
+			waypoints = path:GetWaypoints()
+			blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
+				if blockedWaypointIndex >= nextWaypointIndex then
 					blockedConnection:Disconnect()
+					shit:followPath(destination)
 				end
 			end)
-		end
-		nextWaypointIndex = 2
-		humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-	else warn("Path not computed!", errorMessage) end
+			if not reachedConnection then
+				reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
+					if reached and nextWaypointIndex < #waypoints then
+						nextWaypointIndex = nextWaypointIndex + 1
+						humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+					else
+						reachedConnection:Disconnect()
+						blockedConnection:Disconnect()
+					end
+				end)
+			end
+			nextWaypointIndex = 2
+			humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+		else warn("Path not computed!", errorMessage) end
+	end)
 end
 
 return shit
