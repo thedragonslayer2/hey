047a26457df94b07cf4c6ac1f295173e499093eb
thedragonslayer2 <84556593+thedragonslayer2@users.noreply.github.com>
2021-09-26 16:54:58 +0800

    Update PathFind&WalkTo.lua

--- Misc./PathFind&WalkTo.lua
+++ Misc./PathFind&WalkTo.lua
@@ -3,45 +3,17 @@ local shit = {}
 function shit:GO(pos)
 	local PathfindingService = game:GetService"PathfindingService"
 	local Players = game:GetService"Players"
-	local path = PathfindingService:CreatePath()
 	local player = Players.LocalPlayer
 	local character = player.Character
 	local humanoid = character:WaitForChild"Humanoid"
-	local waypoints
-	local nextWaypointIndex
-	local reachedConnection
-	local blockedConnection
-
-	local function followPath(destination)
-		local success, errorMessage = pcall(function()
-			path:ComputeAsync(character.PrimaryPart.Position, destination)
-		end)
-		if success and path.Status == Enum.PathStatus.Success then
-			waypoints = path:GetWaypoints()
-			blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
-				if blockedWaypointIndex >= nextWaypointIndex then
-					blockedConnection:Disconnect()
-					followPath(destination)
-				end
-			end)
-			if not reachedConnection then
-				reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
-					if reached and nextWaypointIndex < #waypoints then
-						nextWaypointIndex = nextWaypointIndex + 1
-						humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-					else
-						reachedConnection:Disconnect()
-						blockedConnection:Disconnect()
-					end
-				end)
-			end
-			nextWaypointIndex = 2
-			if waypoints[nextWaypointIndex].Action == Enum.PathWaypointAction.Jump then humanoid:ChangeState(Enum.HumanoidStateType.Jumping) end
-			humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-		else warn("Path not computed!", errorMessage) end
+	local path = PathfindingService:CreatePath()
+	path:ComputeAsync(character.PrimaryPart.Position, pos)
+	local waypoints = path:GetWaypoints()
+	for i, v in next, waypoints do
+		if v.Action == Enum.PathWaypointAction.Jump then humanoid:ChangeState(Enum.HumanoidStateType.Jumping) end
+		humanoid:MoveTo(v.Position)
+		humanoid.MoveToFinished:Wait()
 	end
-
-	followPath(pos)
 end
 
 return shit
