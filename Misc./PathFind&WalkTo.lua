3232776c74905a5fe93e042caebde26e6142e6c9
thedragonslayer2 <84556593+thedragonslayer2@users.noreply.github.com>
2021-09-29 11:33:01 +0800

    Update PathFind&WalkTo.lua

--- Misc./PathFind&WalkTo.lua
+++ Misc./PathFind&WalkTo.lua
@@ -17,18 +17,18 @@ function shit:Find(part)
 	path:ComputeAsync(character.PrimaryPart.Position, part.Position)
 	local waypoints = path:GetWaypoints()
 	spawn(function()
-		while wait() and waypoints and shit.toggle and part:IsDescendantOf(workspace) do
-			humanoid.Sit = false
-			humanoid.Jump = true
-		end
+		pcall(function()
+			while wait() and waypoints and shit.toggle and part:IsDescendantOf(workspace) do
+				humanoid.Sit = false
+				humanoid.Jump = true
+			end
+		end)
 	end)
 	pcall(function()
 		for i, v in next, waypoints do
 			if not shit.toggle or not part:IsDescendantOf(workspace) then break end
-			pcall(function()
-				humanoid:MoveTo(v.Position)
-				humanoid.MoveToFinished:Wait()
-			end)
+			humanoid:MoveTo(v.Position)
+			humanoid.MoveToFinished:Wait()
 		end
 	end)
 	waypoints = nil
