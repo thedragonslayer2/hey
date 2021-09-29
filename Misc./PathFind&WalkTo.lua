c0efadbb9d36d85ced50bdd91277321d291cafda
thedragonslayer2 <84556593+thedragonslayer2@users.noreply.github.com>
2021-09-29 11:21:08 +0800

    Update PathFind&WalkTo.lua

--- Misc./PathFind&WalkTo.lua
+++ Misc./PathFind&WalkTo.lua
@@ -1,25 +1,27 @@
 local shit = { toggle = true }
 
-function shit:Find(pos)
+function shit:Find(part)
 	local PathfindingService = game:GetService"PathfindingService"
 	local Players = game:GetService"Players"
 	local player = Players.LocalPlayer
 	local character = player.Character
 	local humanoid = character:WaitForChild"Humanoid"
 	local path = PathfindingService:CreatePath()
-	path:ComputeAsync(character.PrimaryPart.Position, pos)
+	path:ComputeAsync(character.PrimaryPart.Position, part.Position)
 	local waypoints = path:GetWaypoints()
 	spawn(function()
-		while wait() and waypoints and shit.toggle do
+		while wait() and waypoints and shit.toggle and part:IsDescendantOf(workspace) do
 			humanoid.Sit = false
 			humanoid.Jump = true
 		end
 	end)
 	pcall(function()
 		for i, v in next, waypoints do
-			if not shit.toggle then break end
-			humanoid:MoveTo(v.Position)
-			humanoid.MoveToFinished:Wait()
+			if not shit.toggle or not part:IsDescendantOf(workspace) then break end
+			pcall(function()
+				humanoid:MoveTo(v.Position)
+				humanoid.MoveToFinished:Wait()
+			end)
 		end
 	end)
 	waypoints = nil
